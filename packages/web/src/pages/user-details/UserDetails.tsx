3fa067298a45670944a37b7449c2725adecf4c60
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-09-10 01:52:08 +0530

    [master] user profiles layout introduced

--- packages/web/src/pages/user-details/UserDetails.tsx
+++ packages/web/src/pages/user-details/UserDetails.tsx
@@ -9,21 +9,39 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const UserProfilesContainer = styled.div`
+    display: flex;
+    align-items:center;
+    margin-bottom: 5em;
+    max-width: 100%;
+    overflow: scroll;
+    div {
+        margin: 1em;
+        width: 99%;
+        min-width: 100%;
+    }
+`;
+
 const InputWithBtn = styled.div`
     display: flex;
+    justify-content: center;
 `;
 
 const UserDetailsPage: React.FC = () => {
   return (
     <>
       <Container>
-          <div>
-              <div>
-                User Image
-              </div>
+          <div style={{width: '100%'}}>
+              <UserProfilesContainer>
+                <div>User 1</div>
+                <div>User 2</div>
+                <div>User 3</div>
+                <div>User 4</div>
+                <div>User 5</div>
+              </UserProfilesContainer>
               <InputWithBtn>
                     <div>
-                        <input type="text" />
+                        <input type="text" placeholder="Phone" />
                     </div>
                     <div>
                         Go
