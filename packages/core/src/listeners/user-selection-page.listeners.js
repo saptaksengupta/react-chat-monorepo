585828c9d93472823226b599fca60e1a5a304ef5
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-09-13 17:37:17 +0530

    [master] lerna added to manage dependencies.

--- packages/core/src/listeners/user-selection-page.listeners.js
+++ packages/core/src/listeners/user-selection-page.listeners.js
@@ -2,8 +2,9 @@ import { useContext } from 'react';
 import { UserSelectionContext } from '../context/UserSelectionContext';
 import { UserReducer, USER_ACTIONS } from '../reducers/UserReducer';
 
-const {dispatch} = useContext(UserSelectionContext);
+
 
 export const onUserSelect = (userDetails) => { 
+    const {dispatch} = useContext(UserSelectionContext);
     dispatch({type: USER_ACTIONS.SET_SELECTED_USER, payload: {userDetails}});
 }
